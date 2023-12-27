026fe45fb28a90febd23b567f70eaa9a8f6ff537
Jules <ju.pabst@gmail.com>
2023-12-27 14:58:10 +0100

    designed opening screen

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("button pressed")
+    }
+    
 }
 
